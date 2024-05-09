caf14bf8d8ee1071558dfb4a56f426f469681a3c
Himadri Bhattacharjee <107522312+lavafroth@users.noreply.github.com>
2024-05-09 11:15:07 +0530

    ci: automate creating `new` animation scene
    feat: run the editor after creating a script

--- flake.nix
+++ flake.nix
@@ -12,6 +12,24 @@
           packages = with pkgs; [
             manim
             ffmpeg
+            (writeScriptBin "new" ''
+            cat << EOF > "$1.py"
+            #!/usr/bin/env manim
+            from manim import *
+            import sys
+            import os
+
+            sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+            from custom import CodeTransformer, octicon
+
+
+            class Sc(Scene):
+                def construct(self):
+                    Text.set_default(font="Poppins")
+                    Code.set_default(font="Terminess Nerd Font Propo", style="monokai")
+            EOF
+            $EDITOR "$1.py"
+            '')
           ];
         };
       }
