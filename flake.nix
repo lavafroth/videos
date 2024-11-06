146c78538a503fdf4c0c7218562a22ba1e58d900
Himadri Bhattacharjee <107522312+lavafroth@users.noreply.github.com>
2024-11-06 07:12:29 +0530

    deps: bump flake

--- flake.nix
+++ flake.nix
@@ -3,10 +3,17 @@
 
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs, flake-utils }:
-  flake-utils.lib.eachDefaultSystem
-    (system:
-      let pkgs = nixpkgs.legacyPackages.${system}; in
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
       {
         devShells.default = pkgs.mkShell {
           packages = with pkgs; [
@@ -14,19 +21,19 @@
             ffmpeg-full
             kdenlive
             (writeScriptBin "new" ''
-            cat << EOF > "$1.py"
-            #!/usr/bin/env manim
-            from manim import *
-            from manim import Transform as T, ReplacementTransform as RT
-            from hackermanim import *
-            codefont = "Terminess Nerd Font Propo"
+              cat << EOF > "$1.py"
+              #!/usr/bin/env manim
+              from manim import *
+              from manim import Transform as T, ReplacementTransform as RT
+              from hackermanim import *
+              codefont = "Terminess Nerd Font Propo"
 
-            class Sc(Scene):
-                def construct(self):
-                    Text.set_default(font="Poppins")
-                    Code.set_default(font=codefont, style="monokai")
-            EOF
-            $EDITOR "$1.py"
+              class Sc(Scene):
+                  def construct(self):
+                      Text.set_default(font="Poppins")
+                      Code.set_default(font=codefont, style="monokai")
+              EOF
+              $EDITOR "$1.py"
             '')
           ];
         };
